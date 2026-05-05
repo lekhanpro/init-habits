5f0170dd3d5e33397fa49948883ab59a947f51af
lekhan <lekhan2007@gmail.com>
2026-05-05 21:33:11 +0530

    ci: remove hardcoded ndkVersion, add local.properties generation, capture build errors

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 android {
     namespace = "com.habittracker"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
